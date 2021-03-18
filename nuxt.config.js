710cd516eb2cc97d2eec890aa92f4bb3bf46e628
RobertMiski <72018451+RobertMiski@users.noreply.github.com>
2021-03-18 18:20:54 +0000

    da

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
+  
   target: 'static',
+  generate: {
+    fallback: true
+  },
   apollo: {
     clientConfigs: {
       default: {
