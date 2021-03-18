6c8b30d1118dc22f1d64bb6a935e05cf801c1235
RobertMiski <72018451+RobertMiski@users.noreply.github.com>
2021-03-18 18:08:22 +0000

    added everything

---
+++ nuxt.config.js
@@ -0,0 +1,54 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: process.env.BACKEND_URL || "https://localhost:1337/graphql"
+      }
+    }
+  },
+  env: {
+    storeUrl: process.env.STORE_URL || "http://localhost:1337"
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'stocks',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    ['@nuxtjs/apollo',
+    '@nuxtjs/axios']
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="homepage">
+    <h1>Cafetaria El Patron</h1>
+    <div class="article" v-for="article1 in article" v-bind:key="article1">
+      <h2> {{ article1.name }} </h2>
+      <p> {{ article1.category }} </p>
+      <p> {{ article1.price }} </p>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+const _ = require('lodash')
+//  your spreadsheet has to be PUBLIC and SHARED with everybody to be accessed this way
+//  https://sheets.googleapis.com/v4/spreadsheets/{SPREASHEET_ID}/values/{SHEET_TAB_NAME}!{CELLS}?key={GOOGLE_API_KEY}
+const url = 'https://spreadsheets.google.com/feeds/list/1QuNTaRm_mgvSwVyaZgLQ9XgaHPdVVnKSswNa02ruk0Y/od6/public/values?alt=json'
+export default {
+  async asyncData () {
+      const res  = await fetch("https://spreadsheets.google.com/feeds/list/1QuNTaRm_mgvSwVyaZgLQ9XgaHPdVVnKSswNa02ruk0Y/od6/public/values?alt=json");
+      const data = await res.json();
+      const data2 = data.feed.entry;
+      console.log(data2);
+      let article = [];
+      data2.forEach(numbers => {
+        let price = numbers.gsx$number.$t;
+        let name = numbers.gsx$name.$t;
+        let category = numbers.gsx$category.$t;
+        let image = numbers.gsx$image.$t;
+        let id = numbers.gsx$id.$t;
+
+        let info = { 
+        category:category,
+        price: price,
+        name:name,
+      
+        image:image
+      
+      
+      
+    }
+      article.push(info);
+
+      });
+    return {article}
+  }
+}
+</script>
+
+<style>
+#homepage {
+  padding-top: 10vh;
+  max-width: 70vw;
+  margin: auto;
+}
+#homepage h1 {
+  padding-bottom: 5vh;
+}
+#homepage .article {
+  padding-bottom: 5vh;
+}
+</style>
